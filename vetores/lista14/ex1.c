ccf4610e78845389717c993f0cd01e9b70cb3159
Douglas <douglas@alexandria.solar>
2022-05-04 14:48:01 -0300

    lista13 ex1

---
+++ vetores/lista14/ex1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+// limite do vetor
+#define N 80 
+
+int main()
+{
+	int i, qtQuestao, count=0;
+
+	// declarando vetores tipo char
+	char gabarito[N], respostas[N];
+	
+	// ler do teclado a quantidade de questao
+	printf("Informe o quantidade de questoes:\n");
+	scanf("%d", &qtQuestao);
+
+	printf("Informe o gabarito:\n");
+	// armazenar char no vetor de gabarito
+	for(i=0 ; i<qtQuestao ; i++)
+	{
+		scanf("%s", &gabarito[i]);
+	}
+
+	// armazenar char no vetor de respostas
+	printf("Informe as respostas:\n");
+	for(i=0 ; i<qtQuestao ; i++)
+	{
+		scanf("%s", &respostas[i]);
+	}
+
+	// comparar e informar os valores coincidentes
+	for(i=0 ; i<qtQuestao ; i++)
+	{
+		if(gabarito[i]==respostas[i])
+			count++;
+	}
+	printf("quantidades de respostas coincidentes:%d\n", count);
+}
+
