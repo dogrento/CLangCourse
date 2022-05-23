64ab6bf56ce3208cf7beeed49130529e1352d617
Douglas <douglas@alexandria.solar>
2022-05-23 13:41:18 -0300

    ex3 lista 14

--- vetores/fix16/fix2.c
+++ vetores/fix16/fix2.c
@@ -2,36 +2,61 @@
 #include <stdlib.h>
 #include <time.h>
 
+int* cria_vetor(int tam);
+int* multiplica_elementos();
+
 int main()
 {
 	int i, tam1,tam2;
-	srand(time(NULL));
 	// declarar dois ponteiros para buffer alocado
-	int* vetor1, *vetor2;
+	int *vetor1, *vetor2, *vetor3;
 
 	// definir tam1 e tam2
 	printf("Informe o tam1 e tam2");
 	scanf("%d %d", &tam1, &tam2);
+	
+	vetor1 = cria_vetor(tam1);
+	vetor2 = cria_vetor(tam2);
+	vetor3 = multiplica_elementos(vetor1, vetor2, tam1, tam2);
 
-	// funç que irá alocar o espaço 
-	vetor1 = (int*) malloc(tam1 * sizeof(int));
-	vetor2 = (int*) malloc(tam2 * sizeof(int));	
-
-	// usar espaços alocados
-	for(i=0;i<tam1;i++)
+	for(i=0;i<tam1+tam2;i++)
 	{
-		vetor1[i] = rand()%10;
-		printf("vetor1[%d]:%d\n",i, vetor1[i]);
+		printf("vetor3[%d]:%d\n",i, vetor3[i]);
 	}
-	for(i=0;i<tam2;i++)
-	{
-		vetor2[i] = rand()%10;
-		printf("vetor2[%d]:%d\n",i, vetor2[i]);
-	}
-
 	// liberar espaços
 	free(vetor1);
 	free(vetor2);
+	free(vetor3);
 
 	return 0;
+}
+
+int* cria_vetor(int tam)
+{
+	int i;
+	int* vetor;
+
+	srand(time(NULL));
+
+	vetor = (int*) malloc(tam*sizeof(int));
+
+	for(i=0;i<tam;i++)
+	{
+		vetor[i] = rand()%10;
+	}
+	return vetor;
+}
+
+int* multiplica_elementos(int* vetor1, int* vetor2, int tam1, int tam2)
+{
+	int i, tam = tam1+tam2;
+	int* vetor;
+
+	vetor = (int*)malloc(tam*sizeof(int));
+
+	for(i=0;i<tam;i++)
+	{
+		vetor[i] = vetor1[i]*vetor2[i];
+	}
+	return vetor;
 }
